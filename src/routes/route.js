c35429d70ab5a6eaf5b01cb3b62fcf9c4f8e5419
Arkodeep333 <arkojaihind@gmail.com>
2021-12-30 16:16:19 +0530

    feature4 completed

--- src/routes/route.js
+++ src/routes/route.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 const usercontroller=require("../controllers/userController")
 const cartcontroller=require("../controllers/cartController")
-const Reviewcontroller=require("../controllers/orderController")
+const orderController=require("../controllers/orderController")
 const Middleware=require("../middleware/Authentication")
 const ProductController = require("../controllers/productController")
 
@@ -25,4 +25,7 @@ router.post('/users/:userId/cart', Middleware.Auth,cartcontroller.createCart)
 router.put('/users/:userId/cart',Middleware.Auth ,cartcontroller.updateCart)
 router.get('/users/:userId/cart', Middleware.Auth,cartcontroller.getCart)
 router.delete('/users/:userId/cart', Middleware.Auth,cartcontroller.deleteCart)
+// Order APIs
+router.post('/users/:userId/orders', Middleware.Auth,orderController.createOrder)
+router.put('/users/:userId/orders', Middleware.Auth, orderController.updateOrder)
 module.exports = router;
